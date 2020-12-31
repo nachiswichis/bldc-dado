abb1e646e6018a6219c2d09d53aaecb0ace61209
Dado Mista <dadomista@gmail.com>
2020-12-31 10:25:59 -0800

    v0.9 version of CFOC
    The only difference to FW v1.0 is that current gain is twice as high,
    since the resistors of the v0.9 are twice as large.
    
    Signed-off-by: Dado Mista <dadomista@gmail.com>

--- hwconf/hw_Cheap_FOCer_2.h
+++ hwconf/hw_Cheap_FOCer_2.h
@@ -20,7 +20,7 @@
 #ifndef HW_Cheap_FOCer_2
 #define HW_Cheap_FOCer_2
 
-#define HW_NAME					"Cheap FOCer 2"
+#define HW_NAME					"Cheap FOCer 2 v0.9"
 
 // HW properties
 #define HW_HAS_DRV8301
@@ -103,7 +103,7 @@
 #define VIN_R2					2200.0
 #endif
 #ifndef CURRENT_AMP_GAIN
-#define CURRENT_AMP_GAIN		10
+#define CURRENT_AMP_GAIN		20
 #endif
 #ifndef CURRENT_SHUNT_RES
 #define CURRENT_SHUNT_RES		0.0005
