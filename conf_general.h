8700413e865627318bea29fcce1ee93d473bc66b
Dado Mista <dadomista@gmail.com>
2020-12-30 21:42:30 -0800

    Select CFoC2
    Signed-off-by: Dado Mista <dadomista@gmail.com>

--- conf_general.h
+++ conf_general.h
@@ -72,10 +72,10 @@
 // Mark3 version of HW60 with power switch and separate NRF UART.
 //#define HW60_IS_MK3
 //#define HW60_IS_MK4
-#define HW60_IS_MK5
+//#define HW60_IS_MK5
 
-#define HW_SOURCE "hw_60.c"
-#define HW_HEADER "hw_60.h"
+//#define HW_SOURCE "hw_60.c"
+//#define HW_HEADER "hw_60.h"
 
 //#define HW_SOURCE "hw_r2.c"
 //#define HW_HEADER "hw_r2.h"
@@ -103,7 +103,7 @@
 
 // Second revision with separate UART for NRF51
 //#define HW75_300_REV_2
-#define HW75_300_REV_3
+//#define HW75_300_REV_3
 
 //#define HW_SOURCE "hw_75_300.c"
 //#define HW_HEADER "hw_75_300.h"
@@ -155,8 +155,8 @@
 //#define HW_SOURCE "hw_stormcore_100s.c"
 //#define HW_HEADER "hw_stormcore_100s.h"
 
-//#define HW_SOURCE "hw_Cheap_FOCer_2.c"
-//#define HW_HEADER "hw_Cheap_FOCer_2.h"
+#define HW_SOURCE "hw_Cheap_FOCer_2.c"
+#define HW_HEADER "hw_Cheap_FOCer_2.h"
 
 //#define HW_SOURCE "hw_140_300.c"
 //#define HW_HEADER "hw_140_300.h"
